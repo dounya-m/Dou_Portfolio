668ce92cfa32170eeca422aa1ceafc9cb5483129
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-05-06 18:49:30 +0100

    create portfolio pages

---
+++ src/pages/Contatct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contatct() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contatct
--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,1 +1,6 @@
 export {default as Home} from './Home'
+export {default as About} from './About'
+export {default as Services} from './Services'
+export {default as Skills} from './Skills'
+export {default as Contatct} from './Contatct'
+export {default as Eroor} from './Eroor'
