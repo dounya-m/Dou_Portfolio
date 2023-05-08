92fb631e1c07c025da30f26e0f52050784952f18
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-05-09 00:42:38 +0100

    navbar title

--- src/layout/navbar/NavBarComponent.jsx
+++ src/layout/navbar/NavBarComponent.jsx
@@ -1,21 +1,60 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import {AiOutlineHome} from 'react-icons/ai'
 import {RxPerson} from 'react-icons/rx'
 import {MdDesignServices} from 'react-icons/md'
-import {GrServices} from 'react-icons/gr'
+import {AiOutlineQrcode} from 'react-icons/ai'
 import {TbAlignBoxLeftTop} from 'react-icons/tb'
-import {BsMailbox2} from 'react-icons/bs'
+import {AiOutlineMessage} from 'react-icons/ai'
+
 
 
 function NavBarComponent() {
   return (
-    <div>
-        <AiOutlineHome />
-        <RxPerson />
-        <MdDesignServices />
-        <GrServices />
-        <TbAlignBoxLeftTop />
-        <BsMailbox2 />
+    <div className=' text-xl flex flex-col gap-4 border-[1px] border-slate-600 px-2 py-4 rounded-full '>
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="home">
+          <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>Home
+          <span className='absolute w-2 h-4 mt-1 ml-[0.9rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <AiOutlineHome />
+        </NavLink>
+        
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="about">
+          <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>About
+          <span className='absolute w-2 h-4 mt-1 ml-[0.8rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <RxPerson />
+        </NavLink>
+        
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="skill">
+        <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>Skills
+          <span className='absolute w-2 h-4 mt-1 ml-[1.10rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <MdDesignServices />
+        </NavLink>
+        
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="services">
+        <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>Services
+          <span className='absolute w-2 h-4 mt-1 ml-[0.4rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <AiOutlineQrcode/>
+        </NavLink>
+        
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="portfolio">
+        <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>Portfolio
+          <span className='absolute w-2 h-4 mt-1 ml-[0.3rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <TbAlignBoxLeftTop />
+        </NavLink>
+        
+        <NavLink className='relative flex items-center no-underline text-slate-600 hover:text-emerald-500 icone_home' data-hover='hello' to="contact">
+        <span className='absolute py-[0.20rem] text-sm font-semibold text-white rounded-md bg-gray-800 w-[5rem] text-center  right-10  title_home'>Contact
+          <span className='absolute w-2 h-4 mt-1 ml-[0.5rem] rotate-45 bg-gray-800 '></span>
+          </span>
+          <AiOutlineMessage />
+        </NavLink>
+
     </div>
   )
 }
