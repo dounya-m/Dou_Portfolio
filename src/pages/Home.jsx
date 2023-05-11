b9235a601d150ba58cd0acb7447e1cc0a1aee877
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-05-11 23:33:42 +0100

    start home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {AiOutlineHome} from 'react-icons/ai'
 
 function Home() {
   return (
     <>
-    <h1>Home</h1>
+    <div className='flex items-center gap-2 text-xs uppercase border-[1px] py-1 px-2 rounded-full text-gray-200 border-gray-400 '>
+      <AiOutlineHome />
+      introduce
+    </div>
+    <div>
+      <h1>
+        Hey, I'm <span className='text-blue-500'>Dounya</span>
+        
+      </h1>
+    </div>
     </>
   )
 }
