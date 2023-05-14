b6e389966f318a3a396fb53bb8f530e27d7e092c
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-05-14 23:32:59 +0100

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,17 @@ import SubtitleComponent from '../components/subtitle/SubtitleComponent'
 
 function Home() {
   return (
-    <div className='h-full'>
+    <div className='h-full '>
       <div className='w-[20%]'>
       <SubtitleComponent text='Home' icon={AiOutlineHome} />
       </div>
     <div>
-      <h1 className='text-6xl font-light tracking-wide'>
+      <h1 className='text-6xl font-light tracking-wide my-[6vh]'>
         Hey, I'm <span className='name'>Dounya</span>
         <br /> I'm a fullstack developer
         <br /> and a UI/UX designer
       </h1>
-      <p className='text-gray-400 w-[60%] py-5'>
+      <p className='text-gray-400 w-[60%] '>
         I design and code beautifully simple things and i love what i do. Just simple like that!
       </p>
     </div>
