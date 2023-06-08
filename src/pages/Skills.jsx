fc587bb027d70ef9a325d37d5da7eaa8419436d8
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-06-08 22:17:54 +0100

    skill update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdDesignServices } from 'react-icons/md'
 import SubtitleComponent from '../components/subtitle/SubtitleComponent'
+import {FaFigma} from 'react-icons/fa'
 
 function Skills() {
   return (
@@ -8,8 +9,11 @@ function Skills() {
       <SubtitleComponent text='My skills'  icon={MdDesignServices}/>
       <div className='  tracking-wide my-[6vh]'>
         <h2 className='font-light'>
-        My<span className='font-semibold text-rose-500'> Advantages</span>
+          My<span className='font-semibold text-rose-500'> Advantages</span>
         </h2>
+      </div>
+      <div>
+      <FaFigma></FaFigma>
       </div>
     </div>
   )
