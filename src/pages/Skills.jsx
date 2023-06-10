8d655c428850c180c06b378af0d60317060ceb84
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-06-10 10:24:26 +0100

    skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,6 +4,11 @@ import SubtitleComponent from '../components/subtitle/SubtitleComponent'
 import {FaFigma} from 'react-icons/fa'
 
 function Skills() {
+
+  const itemes = [
+    {icon: <FaFigma />, name: 'FaFigma', persentage: '85%' }
+  ]
+  
   return (
     <div>
       <SubtitleComponent text='My skills'  icon={MdDesignServices}/>
@@ -12,9 +17,15 @@ function Skills() {
           My<span className='font-semibold text-rose-500'> Advantages</span>
         </h2>
       </div>
-      <div>
-      <FaFigma></FaFigma>
-      </div>
+      {itemes.map((item) => (
+        <section>
+        <div className='flex flex-col justify-center border-[2px] px-[2rem] py-6 rounded-full'>
+          <div className='text-[4rem]'>{item.icon}</div>
+          <p>{item.name}</p>
+          </div>
+          <p>Figma</p>
+        </section>
+      ))}
     </div>
   )
 }
